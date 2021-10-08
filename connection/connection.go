c046eae3bd53536568ee5ab8e05358e6abcf0a23
Tushar S Agrawal <tusharsagrawal16@gmail.com>
2021-10-09 01:53:09 +0530

    mongo connection moved to seperate package

--- connection/connection.go
+++ connection/connection.go
@@ -1,5 +1,36 @@
 package connection
 
-func Connect() {
+import (
+	"context"
+	"fmt"
+	"time"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var UserCollection *mongo.Collection
+var PostCollection *mongo.Collection
+
+func Connect() (*mongo.Client, context.Context) {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://task:1234567890@cluster0.a3iuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	if err != nil {
+		panic(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Ping Successfull to MongoDB")
+	UserCollection = client.Database("taskDB").Collection("user")
+	PostCollection = client.Database("taskDB").Collection("post")
+
+	return client,ctx
 }
--- main.go
+++ main.go
@@ -4,46 +4,28 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"time"
 
+	"github.com/tusharyaar/task/connection"
 	"github.com/tusharyaar/task/handlers"
 	"github.com/tusharyaar/task/models"
 
 	// MongoDb
-
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func main() {
-
-	// Initialize Mongo DB
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://task:1234567890@cluster0.a3iuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
-	if err != nil {
-		panic(err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Connected to MongoDB")
-
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-    panic(err)
-}
-	fmt.Println("Ping Successfull to MongoDB")
 	var user models.User
-	collection := client.Database("taskDB").Collection("user")
-	err = collection.FindOne(context.TODO(), bson.D{{"email","email@gmail.com"}}).Decode(&user)
+	// Initialize Mongo DB
+	client,ctx := connection.Connect()
+	err := connection.UserCollection.FindOne(context.TODO(), bson.D{{"email","email@gmail.com"}}).Decode(&user)
 	
+	if err != nil {
+		fmt.Println("Error while fetching user")
+	}
+
 	fmt.Println(user)
 	// responds to GET /user
 	http.HandleFunc("/",handlers.GetUser)
-
 	// responds to POST /user
 	http.HandleFunc("/user",handlers.CreateUser)
 
@@ -51,7 +33,6 @@ func main() {
 
 
 // These Run after the server closes
-	defer cancel()
 	defer client.Disconnect(ctx)
 
 
